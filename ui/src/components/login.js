62e9686b5d3ef7efea52d0eb6a40770b55d233d2
gandharkulkarni <gakulkarni@dons.usfca.edu>
2024-04-06 10:56:41 -0700

    Added icon in login page

--- ui/src/components/login.js
+++ ui/src/components/login.js
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { API_HOST, SIGNIN } from '../constants';
 import { Link, useNavigate } from 'react-router-dom';
-
+import icon from '../static/png/icon.png';
 const Login = ({auth}) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -46,11 +46,14 @@ const Login = ({auth}) => {
     }
     return (
         <div className='flex flex-col items-center justify-center h-screen'>
+            <img src={icon} height={700} width={700} alt="icon"/>
+            
             {error && (
-            <div role="alert" className="alert alert-error w-1/3 mb-5">
+            <div role="alert" className="alert alert-error w-1/3 mt-5 mb-5">
                 <span>Error! {error}</span>
             </div>
             )}
+            
             <label className="input input-bordered flex items-center gap-2 mb-5 w-1/3">
                 <EmailIcon />
                 <input type="text" className="grow" placeholder="Email" onChange={(e)=>{setEmail(e.target.value)}} />
@@ -60,6 +63,7 @@ const Login = ({auth}) => {
                 <input type="password" className="grow" onChange={(e)=>{setPassword(e.target.value)}}/>
             </label>
             <Button className="btn btn-active btn-success w-1/3 text-white" onClick={handleSubmit}>Login</Button>
+            <br/>
             <p>Don't have an account? <Link to="/register">Register</Link></p>
         </div>
     );
