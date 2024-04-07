aa1b35c0420248f6099192ddd8551b7930cfeb54
gandharkulkarni <gakulkarni@dons.usfca.edu>
2024-04-06 23:20:30 -0700

    minor changes

--- ui/src/components/myListingCard.js
+++ ui/src/components/myListingCard.js
@@ -2,7 +2,7 @@
 export const MyListingCard = ({imgSrc, address, rent}) => {
     return (
         <div className="card lg:card-side bg-base-100 shadow-xl">
-        <figure><img src={imgSrc} alt="Album"/></figure>
+        <figure><img src={imgSrc} alt="listing"/></figure>
         <div className="card-body">
             <h2 className="card-title">{address}</h2>
             <p>Rent : ${rent} </p>
