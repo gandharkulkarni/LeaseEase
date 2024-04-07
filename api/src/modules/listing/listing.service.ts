58250bf37958f69e717fc2ca3f2d8fd322d31826
vinayusfca <109916498+vinayusfca@users.noreply.github.com>
2024-04-07 00:20:38 -0700

    Added viewListingTrivia.

--- api/src/modules/listing/listing.service.ts
+++ api/src/modules/listing/listing.service.ts
@@ -4,6 +4,7 @@ import { JwtGenerator } from "../../../commons/utils";
 import config from "../../appConfig";
 import { Listing } from "../../../commons/models";
 import { LLM } from "../../../commons/utils";
+import { ListingTrivia } from "../../../commons/models/listingTrivia.entity";
 
 class ListingService {
     public async addListing(listing: any, next: CallableFunction) {
@@ -45,6 +46,21 @@ class ListingService {
             else { // view all listings except mine.
                 const allListings = await new CrudOperations(Listing).getAllDocuments({ user_id: { $nin: [query.user_id] },  "isDeleted":false }, {}, { pageNo: 0, limit: 0 });
                 return next(null, allListings);
+            }
+
+        } catch (error) {
+            console.log("viewListings error: " + error);
+        }
+    }
+
+    public async viewListingTrivia(query: any, next: CallableFunction) {
+        try {//query = {id: ""}
+            if (query.id){ // view my Listing
+                const myListings = await new CrudOperations(ListingTrivia).getDocument({listing_id: query.id, "isDeleted":false},{})
+                return next(null, myListings);
+            }
+            else { // view all listings except mine.
+               return next(null, "Error viewing listing trivia.");
             }
 
         } catch (error) {
