66449cbbb94c47e2792119aa14b336b0b1867b54
gandharkulkarni <gakulkarni@dons.usfca.edu>
2024-04-07 02:10:16 -0700

    new api endpoint

--- ui/src/constants.js
+++ ui/src/constants.js
@@ -4,4 +4,4 @@ export const SIGNUP = 'public/api/v1/auth/signup';
 export const GET_LISTING = 'private/api/v1/listing/viewListings';
 export const DELETE_LISTING = 'private/api/v1/listing/deleteListing';
 export const ADD_LISTING = 'private/api/v1/listing/addListing';
-
+export const GET_LISTING_TRIVIA = 'private/api/v1/listing/viewListingTrivia'
