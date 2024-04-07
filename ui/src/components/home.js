20cfd1ad2c46241932d540cc6907f80bd14761b1
gandharkulkarni <gakulkarni@dons.usfca.edu>
2024-04-06 19:13:50 -0700

    refactor home page code

--- ui/src/components/home.js
+++ ui/src/components/home.js
@@ -2,24 +2,24 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { ICON } from "../static/png/icon";
 import { ListingTabs } from "./listingTabs";
+import { Navbar } from "./navbar";
 
-const Home = ({auth}) => {
+const Home = ({ auth }) => {
     let navigate = useNavigate();
-    useEffect(()=>{
-        if(!auth.isAuthenticated()){
+    useEffect(() => {
+        if (!auth.isAuthenticated()) {
             //navigate('/login')
         }
     })
-    
-    return(
-        <div className='flex flex-col items-center justify-center'>
-            <div className="h-1/3 w-1/3">
-                <ICON />
-            </div>
-            <ListingTabs />
 
+    return (
+        <div>
+            <Navbar auth={auth} />
+            <div className='flex flex-col items-center justify-center mt-20'>
+                <ListingTabs />
+            </div>
         </div>
     );
-  };
+};
 
-  export default Home;
+export default Home;
