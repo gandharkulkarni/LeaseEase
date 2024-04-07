9601f08646842175f1883e141830793888aa53b6
gandharkulkarni <gakulkarni@dons.usfca.edu>
2024-04-06 21:22:15 -0700

    View listings new layout

--- ui/src/components/viewListings.js
+++ ui/src/components/viewListings.js
@@ -1,6 +1,10 @@
-export const ViewListings = () =>{
+import { Navbar } from "./navbar"
+import { ListingTabs } from "./listingTabs"
+export const ViewListings = ({auth}) =>{
     return (
         <div>
+            <Navbar auth={auth} />
+            <ListingTabs allListingTab={true}/>
             <h1> View Listings</h1>
         </div>
     )
