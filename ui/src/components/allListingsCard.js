7998d3073367f1ceee4b7aea6283fd937935405f
gandharkulkarni <gakulkarni@dons.usfca.edu>
2024-04-07 02:10:41 -0700

    Modal api integration

--- ui/src/components/allListingsCard.js
+++ ui/src/components/allListingsCard.js
@@ -1,4 +1,33 @@
-export const ListingCard = ({ imgSrc, address, rent }) => {
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { API_HOST, GET_LISTING_TRIVIA } from "../constants";
+
+export const ListingCard = ({ _id, imgSrc, address, rent, auth }) => {
+    const [restaurants, setRestaurant] = useState(null);
+    const [grocery, setGrocery] = useState(null);
+    const [tourist, setTourist] = useState(null);
+    const [streetAddress, setStreetAddress] = useState(null);
+    const [listingTrivia, setListingTrivia] = useState(null);
+    useEffect(() => {
+        if (streetAddress && restaurants && grocery && tourist) {
+            fetchListingTrivia()
+        }
+    })
+    const fetchListingTrivia = async () => {
+        let response = await axios.get(API_HOST + GET_LISTING_TRIVIA, {
+            headers: {
+                Authorization: auth.getToken() //the token is a variable which holds the token
+            },
+            params: {
+                id: _id
+            }
+        })
+        setListingTrivia(response?.data?.result);
+        setStreetAddress(response?.data?.result?.property_addr);
+        setGrocery(response?.data?.result?.grocery);
+        setRestaurant(response?.data?.result?.restaurant_places);
+        setTourist(response?.data?.result?.tourist_places);
+    }
     return (
         <div>
             <div className="card w-68 glass">
@@ -12,28 +41,53 @@ export const ListingCard = ({ imgSrc, address, rent }) => {
                     </div>
                 </div>
             </div>
+
             <dialog id="listing_modal" className="modal modal-bottom sm:modal-middle">
                 <div className="modal-box">
-                    <h3 className="font-bold text-lg">Hello!</h3>
-                    <p className="py-4">Press ESC key or click the button below to close</p>
-                    <div className="modal-action">
-                        <form method="dialog">
-                            {/* if there is a button in form, it will close the modal */}
-                            <button className="btn">Close</button>
-                        </form>
-                    </div>
+                    {listingTrivia ? (
+                        <div>
+                            <h3 className="font-bold text-lg">Address : {listingTrivia.property_addr}</h3>
+                            <p className="py-4">Owner : {listingTrivia.owner} </p>
+                            <p className="py-4"><b>Tenants : {listingTrivia.tenant}</b></p>
+                            <div className="modal-action">
+                                <form method="dialog">
+                                    <button className="btn">Close</button>
+                                </form>
+                            </div>
+                        </div>
+                    ) : (
+                        <progress className="progress w-56"></progress>
+                    )
+                    }
                 </div>
             </dialog>
             <dialog id="trivia_modal" className="modal modal-bottom sm:modal-middle">
                 <div className="modal-box">
-                    <h3 className="font-bold text-lg">Trivia time!</h3>
-                    <p className="py-4">Press ESC key or click the button below to close</p>
-                    <div className="modal-action">
-                        <form method="dialog">
-                            {/* if there is a button in form, it will close the modal */}
-                            <button className="btn">Close</button>
-                        </form>
-                    </div>
+                    {listingTrivia ? (
+                        <div>
+                            <h3 className="font-bold text-lg">Trivia!</h3>
+                            <h4 className="font-bold text-md">Restautants around here:</h4>
+                            {restaurants.map((name, index) => {
+                                <div key={index} className="badge badge-primary badge-sm"><p className="py-4">{name}</p></div>
+                            })}
+                            <h4 className="font-bold text-md">Grocery outlets around here:</h4>
+                            {grocery.map((name, index) => {
+                                <div key={index} className="badge badge-secondary badge-sm"><p className="py-4">{name}</p></div>
+                            })}
+                            <h4 className="font-bold text-md">Tourist around here:</h4>
+                            {tourist.map((name, index) => {
+                                <div key={index} className="badge badge-accent badge-sm"><p className="py-4">{name}</p></div>
+                            })}
+                            <div className="modal-action">
+                                <form method="dialog">
+                                    <button className="btn">Close</button>
+                                </form>
+                            </div>
+                        </div>
+                    ) : (
+                        <progress className="progress w-56"></progress>
+                    )
+                    }
                 </div>
             </dialog>
         </div>
