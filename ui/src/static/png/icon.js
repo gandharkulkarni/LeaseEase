7b3d1a42d04f8b12dc6514a78e05d94354dc94d4
gandharkulkarni <gakulkarni@dons.usfca.edu>
2024-04-06 19:40:53 -0700

    Added light icon

--- ui/src/static/png/icon.js
+++ ui/src/static/png/icon.js
@@ -1,4 +1,10 @@
 import icon from './icon.png';
-export const ICON = () =>{
-    return <img src={icon} height={700} width={700} alt="icon"/>
+import icon_light from './icon-white.png'
+export const ICON = ({height=700, width=700}) =>{
+    return <img src={icon} height={height} width={width} alt="icon"/>
 }
+
+export const ICON_LIGHT = ({height=700, width=700}) =>{
+    return <img src={icon_light} height={height} width={width} alt="icon_light"/>
+}
+
