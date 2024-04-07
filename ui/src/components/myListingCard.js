63d59d1544228d0c28720a58582d4697b5adefb9
gandharkulkarni <gakulkarni@dons.usfca.edu>
2024-04-07 01:15:12 -0700

    Passing method as props

--- ui/src/components/myListingCard.js
+++ ui/src/components/myListingCard.js
@@ -1,5 +1,6 @@
 
-export const MyListingCard = ({imgSrc, address, rent}) => {
+export const MyListingCard = ({_id, imgSrc, address, rent, handleDelete}) => {
+    
     return (
         <div className="card lg:card-side bg-base-100 shadow-xl">
         <figure><img src={imgSrc} alt="listing"/></figure>
@@ -7,7 +8,7 @@ export const MyListingCard = ({imgSrc, address, rent}) => {
             <h2 className="card-title">{address}</h2>
             <p>Rent : ${rent} </p>
             <div className="card-actions justify-end">
-            <button className="btn btn-accent">Delete</button>
+            <button className="btn btn-accent" onClick={()=>handleDelete(_id)}>Delete</button>
             </div>
         </div>
         </div>
